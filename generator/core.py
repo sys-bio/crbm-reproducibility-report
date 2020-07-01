6bd28c5687f9ead488fd1709f7f12aa323789244
David Nickerson <david.nickerson@gmail.com>
2020-07-01 23:04:02 +1200

    adding an example report JSON file and using that to render the template

--- generator/core.py
+++ generator/core.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import os.path
 from .ReportGenerator import ReportGenerator
 
@@ -17,15 +18,22 @@ def generate_report(input_file, template_directory, output_directory):
     print("Generating the CRBM Reproducibility Report from: " + input_file +
           "; with output stored in the directory: " + output_directory)
 
-    generator = ReportGenerator(template_directory)
-    template = generator.get_template("template.tex")
+    # load the input file
+    report_description = {}
+    with open(input_file, "r") as read_file:
+        report_description = json.load(read_file)
+    # need to add in the logo for our template
     logo_image_path = os.path.join(template_directory, "figures", "CRBM-logo.png")
-    rendered_tex = template.render(title="Title goes here",
-                                   logo_image=os.path.abspath(logo_image_path).replace('\\', '/'),
-                                   journal="PLoS Computational Biology",
-                                   journal_submission_id="PCOMPBIOL-D-20-1234",
-                                   curation_outcome_summary="This is awesome and short.",
-                                   summary_comments="This would be a longer bit of text.")
+    report_description["logo_image"] = os.path.abspath(logo_image_path).replace('\\', '/')
+
+    # create a generator that can locate our template directory
+    generator = ReportGenerator(template_directory)
+
+    # and fetch the report template
+    template = generator.get_template("template.tex")
+
+    # process the template using the information from the input file
+    rendered_tex = template.render(report_description)
     #print(rendered_tex)
     generator.compile_tex(rendered_tex, "bob.pdf")
 
